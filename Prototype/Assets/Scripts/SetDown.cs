ec95266b1841864cbd13f97412643b0cf4224e33
Chirapapan <tapsangwan@gmail.com>
2015-10-29 10:13:52 +0100

    Implement Leap motion #1
    Change the position of AED and import the script of leap motion input

---
+++ Prototype/Assets/Scripts/SetDown.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetDown : MonoBehaviour {
+
+	public Vector3 setDownPosition;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (gameObject.tag == "Trap") {
+			Debug.Log("Stay with me");
+
+		}
+	}
+
+
+}
