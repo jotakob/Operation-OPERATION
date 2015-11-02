255249d38812f10eeaddb8a3fb4032229c14ea58
darkstrauss <alanhart@live.com>
2015-11-02 12:18:20 +0100

    bla

---
+++ Prototype/Assets/OpenAED.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenAED : MonoBehaviour {
+
+	public Animation AEDAnimation;
+
+	bool open;
+
+	void Start()
+	{
+		//AEDAnimation.playAutomatically = false;
+		open = false;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("OI");
+		Debug.Log (other.name);
+		if (!open) {
+			AEDAnimation.Play();
+			open = true;
+		}
+	}
+}
