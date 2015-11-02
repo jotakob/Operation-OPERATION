a48d79fc18b09f837a60cad6b2445c2f65510c15
Alan Hart <alanhart@live.com>
2015-11-02 13:06:09 +0100

    fixed AED anim

--- Prototype/Assets/OpenAED.cs
+++ Prototype/Assets/OpenAED.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class OpenAED : MonoBehaviour {
 
 	public Animation AEDAnimation;
+    public Animator AEDAnimator;
 
 	bool open;
 
@@ -11,7 +12,20 @@ public class OpenAED : MonoBehaviour {
 	{
 		//AEDAnimation.playAutomatically = false;
 		open = false;
+
+        StartCoroutine(PlayAnimation());
 	}
+
+    IEnumerator PlayAnimation()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Debug.Log("Open sesame");
+        //AEDAnimation.Play();
+        AEDAnimator.Play("Open");
+        open = true;
+        yield return new WaitForSeconds(5.0f);
+        AEDAnimator.Play("Close");
+    }
 
 	void OnTriggerEnter(Collider other)
 	{
