da9b90d993d15d710c09620177a7489da218994d
darkstrauss <alanhart@live.com>
2015-11-04 17:28:03 +0100

    asdkghQI

--- Prototype/Assets/Scripts/ActivateAED.cs
+++ Prototype/Assets/Scripts/ActivateAED.cs
@@ -4,7 +4,7 @@ using UnityEngine.Audio;
 using System.Collections.Generic;
 
 public class ActivateAED : MonoBehaviour {
-	public AudioClip beep;
+	public GameObject webcam;
 
 	public bool placedOne;
 	public bool placedTwo;
@@ -22,17 +22,10 @@ public class ActivateAED : MonoBehaviour {
 	void Start()
 	{
 		AEDLines = new Dictionary<int, AudioClip> ();
-		StartCoroutine (LoadAEDLines());
-	}
-
-	IEnumerator LoadAEDLines()
-	{
 		for (int i = 1; i < 25; i++) {
 			AudioClip tmp = Resources.Load("AEDLines/Line" + i) as AudioClip;
 			AEDLines.Add(i, tmp);
 		}
-
-		yield return null;
 	}
 
     void Awake()
@@ -146,9 +139,14 @@ public class ActivateAED : MonoBehaviour {
                     break;
 
 				case 10:
-                case 11:
+				case 11:
+					playSound(state);
+					state++;
+					break;
+
 				case 12:
                     playSound(state);
+					webcam.GetComponent<CamTest>().doAnalysis = true;
                     state++;
                     break;
 
--- Prototype/Assets/Scripts/StartingAnimationClip.cs
+++ Prototype/Assets/Scripts/StartingAnimationClip.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class StartingAnimationClip : MonoBehaviour {
-	
+public class StartingAnimationClip : MonoBehaviour
+{
 	Image imageComponent;
 	public Sprite startScreen;
+	public GameObject HeadRig;
 	public Sprite[] scene1;
 	public Sprite[] scene2;
 	public Sprite[] scene3;
@@ -32,27 +33,30 @@ public class StartingAnimationClip : MonoBehaviour {
 
 		for (int i = 1; i < scene1.Length; i++) {
 			imageComponent.sprite = scene1[i];
-			Debug.Log("Scene1, Frame: " + i);
 			yield return new WaitForSeconds(0.2f);
 		}
 
 		for (int i = 0; i < scene2.Length; i++) {
 			imageComponent.sprite = scene2[i];
-			Debug.Log("Scene2, Frame: " + i);
 			yield return new WaitForSeconds(0.2f);
 		}
 
 		for (int i = 0; i < scene3.Length; i++) {
 			imageComponent.sprite = scene3[i];
-			Debug.Log("Scene3, Frame: " + i);
 			yield return new WaitForSeconds(0.2f);
 		}
 
-		Debug.Log ("Done");
+		for (float i = 0; i < 29; i++) {
+			yield return new WaitForEndOfFrame();
+		}
+
+		HeadRig.GetComponent<OVRScreenFade> ().enabled = true;
 
 		Destroy(blackBox.gameObject);
 		activateScene.SetActive (true);
 		layer.gameObject.SetActive (false);
+
+		yield return new WaitForSeconds (1);
 
 		yield return null;
 	}
